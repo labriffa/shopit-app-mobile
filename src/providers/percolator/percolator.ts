2092e40fb3ef4fd04183d7dc5836a0570decdd32
Lewis Briffa <lewis.a.briffa@gmail.com>
2020-09-01 20:33:04 +0100

    Update percolator.ts
    add environment variable to percolator file.

--- src/providers/percolator/percolator.ts
+++ src/providers/percolator/percolator.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment, SHOPIT_API_ENDPOINT } from '../../environments/environment';
 
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class Percolator {
 
-	readonly API_ENDPOINT = 'http://ec2-35-178-130-149.eu-west-2.compute.amazonaws.com:9000/api';
+	readonly API_ENDPOINT = SHOPIT_API_ENDPOINT;
 	readonly API_SAVE_SEARCHES = '/save-searches';
 
 	constructor(public http: HttpClient) { }
@@ -16,7 +17,6 @@ export class Percolator {
     *  a given search term
 	*/
 	saveSearch(userId, playerId, query) {
-		alert(this.API_ENDPOINT + "" + this.API_SAVE_SEARCHES);
 		return this.http.post(this.API_ENDPOINT + this.API_SAVE_SEARCHES, {
 			userId: userId,
 			playerId: playerId,
